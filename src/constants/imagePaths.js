71d678d2bfe30eb436827ab2942c38e24291b7a4
Chieh <secret12501250@gmail.com>
2026-04-01 16:10:07 +0800

    feat: add weather icons and weather constants (weatherCodes, weatherImages)

---
+++ src/constants/imagePaths.js
@@ -0,0 +1,133 @@
+import clearDay               from "../assets/images/weather_icons/clear-day.svg";
+import clearNight             from "../assets/images/weather_icons/clear-night.svg";
+import partlyCloudyDay        from "../assets/images/weather_icons/partly-cloudy-day.svg";
+import partlyCloudyNight      from "../assets/images/weather_icons/partly-cloudy-night.svg";
+import overcastDay            from "../assets/images/weather_icons/overcast-day.svg";
+import overcastNight          from "../assets/images/weather_icons/overcast-night.svg";
+import fogDay                 from "../assets/images/weather_icons/fog-day.svg";
+import fogNight               from "../assets/images/weather_icons/fog-night.svg";
+import drizzle                from "../assets/images/weather_icons/drizzle.svg";
+import partlyCloudyDayDrizzle from "../assets/images/weather_icons/partly-cloudy-day-drizzle.svg";
+import partlyCloudyNightDrizzle from "../assets/images/weather_icons/partly-cloudy-night-drizzle.svg";
+import sleet                  from "../assets/images/weather_icons/sleet.svg";
+import partlyCloudyDaySleet   from "../assets/images/weather_icons/partly-cloudy-day-sleet.svg";
+import partlyCloudyNightSleet from "../assets/images/weather_icons/partly-cloudy-night-sleet.svg";
+import rain                   from "../assets/images/weather_icons/rain.svg";
+import partlyCloudyDayRain    from "../assets/images/weather_icons/partly-cloudy-day-rain.svg";
+import partlyCloudyNightRain  from "../assets/images/weather_icons/partly-cloudy-night-rain.svg";
+import snow                   from "../assets/images/weather_icons/snow.svg";
+import partlyCloudyDaySnow    from "../assets/images/weather_icons/partly-cloudy-day-snow.svg";
+import partlyCloudyNightSnow  from "../assets/images/weather_icons/partly-cloudy-night-snow.svg";
+import hail                   from "../assets/images/weather_icons/hail.svg";
+import thunderstormsDay       from "../assets/images/weather_icons/thunderstorms-day.svg";
+import thunderstormsNight     from "../assets/images/weather_icons/thunderstorms-night.svg";
+import thunderstormsRain      from "../assets/images/weather_icons/thunderstorms-rain.svg";
+import thunderstormsDayRain   from "../assets/images/weather_icons/thunderstorms-day-rain.svg";
+import thunderstormsNightRain from "../assets/images/weather_icons/thunderstorms-night-rain.svg";
+
+export const weatherIcons = {
+  "clear-day": {
+    alt: "clear sky",
+    src: clearDay,
+  },
+  "clear-night": {
+    alt: "clear sky",
+    src: clearNight,
+  },
+  "partly-cloudy-day": {
+    alt: "partly cloudy",
+    src: partlyCloudyDay,
+  },
+  "partly-cloudy-night": {
+    alt: "partly cloudy",
+    src: partlyCloudyNight,
+  },
+  "overcast-day": {
+    alt: "overcast",
+    src: overcastDay,
+  },
+  "overcast-night": {
+    alt: "overcast",
+    src: overcastNight,
+  },
+  "fog-day": {
+    alt: "fog",
+    src: fogDay,
+  },
+  "fog-night": {
+    alt: "fog",
+    src: fogNight,
+  },
+  "drizzle": {
+    alt: "drizzle",
+    src: drizzle,
+  },
+  "partly-cloudy-day-drizzle": {
+    alt: "drizzle",
+    src: partlyCloudyDayDrizzle,
+  },
+  "partly-cloudy-night-drizzle": {
+    alt: "drizzle",
+    src: partlyCloudyNightDrizzle,
+  },
+  "sleet": {
+    alt: "sleet",
+    src: sleet,
+  },
+  "partly-cloudy-day-sleet": {
+    alt: "sleet",
+    src: partlyCloudyDaySleet,
+  },
+  "partly-cloudy-night-sleet": {
+    alt: "sleet",
+    src: partlyCloudyNightSleet,
+  },
+  "rain": {
+    alt: "rain",
+    src: rain,
+  },
+  "partly-cloudy-day-rain": {
+    alt: "rain",
+    src: partlyCloudyDayRain,
+  },
+  "partly-cloudy-night-rain": {
+    alt: "rain",
+    src: partlyCloudyNightRain,
+  },
+  "snow": {
+    alt: "snow",
+    src: snow,
+  },
+  "partly-cloudy-day-snow": {
+    alt: "snow",
+    src: partlyCloudyDaySnow,
+  },
+  "partly-cloudy-night-snow": {
+    alt: "snow",
+    src: partlyCloudyNightSnow,
+  },
+  "hail": {
+    alt: "hail",
+    src: hail,
+  },
+  "thunderstorms-day": {
+    alt: "thunderstorm",
+    src: thunderstormsDay,
+  },
+  "thunderstorms-night": {
+    alt: "thunderstorm",
+    src: thunderstormsNight,
+  },
+  "thunderstorms-rain": {
+    alt: "thunderstorm with rain",
+    src: thunderstormsRain,
+  },
+  "thunderstorms-day-rain": {
+    alt: "thunderstorm with rain",
+    src: thunderstormsDayRain,
+  },
+  "thunderstorms-night-rain": {
+    alt: "thunderstorm with rain",
+    src: thunderstormsNightRain,
+  },
+};
