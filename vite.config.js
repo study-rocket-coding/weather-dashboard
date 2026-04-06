22a75e8af9e5719c89c3455070c677519d216284
Chieh <secret12501250@gmail.com>
2026-04-06 21:13:23 +0800

    chore: configure Vitest environment and test scripts

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
